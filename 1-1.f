85e4bd962cd214088aa213874d4c0bed4a7b1832
toxicologist <toxiccologist@gmail.com>
2022-08-17 15:33:21 -0300

    1

---
+++ 1-1.f
@@ -0,0 +1,8 @@
+c     programa 1-1
+      write(*,*) 'Insira r1, r2'
+      read(*,*) ar1, ar2
+      pi = acos(-1.e0)
+      write(*,*) 'pi:', pi
+      a_area = 2 * pi * r2 * 2 * pi * r1
+      write(*,*) 'área:', a_area
+      end
